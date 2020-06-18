053efe0332f6e967dfcea2843c0d1d7d56436c7d
KEVALHARKHANI <harkhani45@gmail.com>
2020-06-18 12:55:44 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -3,25 +3,53 @@ from django.db.models import Q
 from .serializers import Foodserializers,compare
 from home.models import food,restro
 from django.contrib import messages
+from django.views.generic import ListView
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 
 
 # Create your views here.
 
-class Search_bar(ListAPIView):
-    model = food
-    queryset = food.objects.all()
-    #template_name = 'search_result.html'
-    serializer_class = Foodserializers
-    filter_backends = (SearchFilter)
-    search_fields=('foodname','restroname')
-
-
-    #def get_queryset(self, **kwargs):
-    #    context=super().get_context_data(**kwargs)
-     #   context['filter']=Searchserializers(self.request.GET,queryset=self.get_queryset())
-      #  return context
+class Search_bar(ListView):
+    def post(self, request, *args, **kwargs):
+        q = str(request.POST['q'])
+        query = q
+        query_list = q.split(' ')
+        food_queryset_list = []
+        restro_queryset_list = []
+        query_error = []
+        for q in query_list:
+            food_queryset = list(food.objects.filter(Q(foodname__icontains=q) |
+                                                     Q(contain__icontains=q) |
+                                                     Q(catagery__icontains=q))
+                                 )
+            if food_queryset:
+                if food_queryset_list and food_queryset:
+                    food_queryset_list = compare(food_queryset_list, food_queryset)
+                else:
+                    food_queryset_list.extend(food_queryset)
+            print('food:', food_queryset_list)
+            restro_queryset = restro.objects.filter(Q(catagery__icontains=q) |
+                                                    Q(detail__icontains=q) |
+                                                    Q(restroname__icontains=q) |
+                                                    Q(address__icontains=q)
+                                                    )
+            if restro_queryset:
+                if restro_queryset_list and restro_queryset:
+                    restro_queryset_list = compare(restro_queryset_list, restro_queryset)
+                else:
+                    restro_queryset_list.extend(restro_queryset)
+                restro_queryset_list.extend(restro_queryset)
+            if not food_queryset and not restro_queryset:
+                query_error.append(q)
+        print('retsro:', restro_queryset_list)
+        if food_queryset_list or restro_queryset_list:
+            return render(request, 'search_result.html',
+                          {'restro': set(restro_queryset_list), 'food': set(food_queryset_list),
+                           'query_error': query_error, 'query': query})
+        else:
+            messages.info(request, 'Product Not found')
+            return render(request, 'search_result.html', {'empty_flage': True, 'query': query})
 
 def search_bar(request):
     q=str(request.POST['q'])
@@ -60,8 +88,6 @@ def search_bar(request):
     else:
         messages.info(request,'Product Not found')
         return render(request,'search_result.html',{'empty_flage':True,'query':query})
-def deep_search(request):
-    return None
 
 
 
